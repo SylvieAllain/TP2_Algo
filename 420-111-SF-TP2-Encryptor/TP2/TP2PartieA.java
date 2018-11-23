9b15368aaf019de92941f5cbd73066a6738b580c
Thierry Légaré <thilega94@gmail.com>
2018-11-23 15:43:29 -0500

    Partie A 2 fonction

--- 420-111-SF-TP2-Encryptor/TP2/TP2PartieA.java
+++ 420-111-SF-TP2-Encryptor/TP2/TP2PartieA.java
@@ -1,13 +1,45 @@
- 
 
 
 public class TP2PartieA
 {
-  static char transformASCII_IntToChar(int value){ 
-      return (char)value;
-  }
+    static int convertFromBinaryToInt(char[] binArray){
+        int value = 0;
+        for (int i = binArray.length - 1; i >= 0; i--){
+            if (binArray[i] == '1'){
+                value = value + getBinValue(binArray.length - i - 1);
+            }
+        }
+        return value;
+    }
+
+    static int getBinValue(int pos){
+        int valueOfPosition = 1;
+        for (int i = 0; i < pos; i++){
+            valueOfPosition = valueOfPosition + valueOfPosition;
+        }
+        return valueOfPosition;
+    }
     
-  static int transformASCII_CharToInt(char c){
-      return (int)c;
-  }   
+    static char[] convertFromIntToBinary(int value,int sizeRequired){
+        char[] charArray = new char[sizeRequired];
+        for(int i = 0; i < sizeRequired; i++){
+            int binPosValue = getBinValue(sizeRequired - 1 - i);
+            if(binPosValue <= value){
+                value = value - binPosValue;
+                charArray[i] = '1';
+            }
+            else{
+                charArray[i] = '0';
+            }
+        }
+        return charArray;
+    }
+    
+    static char transformASCII_IntToChar(int value){ 
+        return (char)value;
+    }
+
+    static int transformASCII_CharToInt(char c){
+        return (int)c;
+    }   
 }
--- 420-111-SF-TP2-Encryptor/TP2/TP2PartieATest.java
+++ 420-111-SF-TP2-Encryptor/TP2/TP2PartieATest.java
@@ -8,8 +8,166 @@ public class TP2PartieATest
     
     // ========== TODO: tests additionnels à faire par l'étudiant
     
-
+    //De la fonction convertFromBinaryToInt
+    @Test
+    public void WHEN_binArrayValueIsZero_THEN_convertFromBinaryToInt_returnsIsZero(){
+        //Arrange        
+        final char[] BINARY_ARRAY = {'0'};
+        
+        //Act
+        int intValue = TP2PartieA.convertFromBinaryToInt(BINARY_ARRAY);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 0;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
     
+    @Test
+    public void WHEN_binArrayValueIsOne_THEN_convertFromBinaryToInt_returnsIsOne(){
+        //Arrange        
+        final char[] BINARY_ARRAY = {'1'};
+        
+        //Act
+        int intValue = TP2PartieA.convertFromBinaryToInt(BINARY_ARRAY);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 1;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    @Test
+    public void WHEN_binArrayValueIsZeroOne_THEN_convertFromBinaryToInt_returnsIsTwo(){
+        //Arrange        
+        final char[] BINARY_ARRAY = {'1','0'};
+        
+        //Act
+        int intValue = TP2PartieA.convertFromBinaryToInt(BINARY_ARRAY);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 2;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    
+    //De la fonction getBinValue
+    @Test 
+    public void WHEN_positionIsZero_THEN_getBinValue_returnsOne(){
+        //Arrange        
+        final int POSITION = 0;
+        
+        //Act
+        int intValue = TP2PartieA.getBinValue(POSITION);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 1;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    @Test
+    public void WHEN_positionIsOne_THEN_getBinValue_returnsTwo(){
+        //Arrange        
+        final int POSITION = 1;
+        
+        //Act
+        int intValue = TP2PartieA.getBinValue(POSITION);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 2;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    @Test
+    public void WHEN_positionIsTwo_THEN_getBinValue_returnsFour(){
+        //Arrange        
+        final int POSITION = 2;
+        
+        //Act
+        int intValue = TP2PartieA.getBinValue(POSITION);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 4;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    @Test
+    public void WHEN_positionIsThree_THEN_getBinValue_returnsHeigh(){
+        //Arrange        
+        final int POSITION = 3;
+        
+        //Act
+        int intValue = TP2PartieA.getBinValue(POSITION);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 8;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    @Test
+    public void WHEN_positionIsSeven_THEN_getBinValue_returns128(){
+        //Arrange        
+        final int POSITION = 7;
+        
+        //Act
+        int intValue = TP2PartieA.getBinValue(POSITION);
+        
+        //Assert
+        final int EXPECTED_INT_VALUE = 128;
+        assertEquals(EXPECTED_INT_VALUE, intValue);
+    }
+    
+    //De la fonction convertIntToBinary
+    @Test
+    public void WHEN_intValueIs_0_THEN_convertFromIntToBinary_returnsBinaryRepresentationOfIntValue(){
+        //Arrange
+        int INT_VALUE = 0;
+        int BINARY_ARRAY_SIZE = 1;
+
+        //Act
+        char[] binaryArray = TP2PartieA.convertFromIntToBinary(INT_VALUE, BINARY_ARRAY_SIZE);
+        
+        //Assert
+        final char[] EXPECTED_BINARY_ARRAY = {'0'};
+        assertArrayEquals(EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    @Test
+    public void WHEN_intValueIs_1_THEN_convertFromIntToBinary_returnsBinaryRepresentationOfIntValue(){
+        //Arrange
+        int INT_VALUE = 1;
+        int BINARY_ARRAY_SIZE = 1;
+
+        //Act
+        char[] binaryArray = TP2PartieA.convertFromIntToBinary(INT_VALUE, BINARY_ARRAY_SIZE);
+        
+        //Assert
+        final char[] EXPECTED_BINARY_ARRAY = {'1'};
+        assertArrayEquals(EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    @Test
+    public void WHEN_intValueIs_2_THEN_convertFromIntToBinary_returnsBinaryRepresentationOfIntValue(){
+        //Arrange
+        int INT_VALUE = 2;
+        int BINARY_ARRAY_SIZE = 2;
+
+        //Act
+        char[] binaryArray = TP2PartieA.convertFromIntToBinary(INT_VALUE, BINARY_ARRAY_SIZE);
+        
+        //Assert
+        final char[] EXPECTED_BINARY_ARRAY = {'1','0'};
+        assertArrayEquals(EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    @Test
+    public void WHEN_intValueIs_3_THEN_convertFromIntToBinary_returnsBinaryRepresentationOfIntValue(){
+        //Arrange
+        int INT_VALUE = 3;
+        int BINARY_ARRAY_SIZE = 2;
+
+        //Act
+        char[] binaryArray = TP2PartieA.convertFromIntToBinary(INT_VALUE, BINARY_ARRAY_SIZE);
+        
+        //Assert
+        final char[] EXPECTED_BINARY_ARRAY = {'1','1'};
+        assertArrayEquals(EXPECTED_BINARY_ARRAY, binaryArray);
+    }
     
     // ========== tests fournis par le professeur
     
@@ -51,7 +209,7 @@ public class TP2PartieATest
         assertEquals(EXPECTED_INT_VALUE, intValue);
     }
     
-    //ConvertFromInToBinary
+    //ConvertFromIntToBinary
     @Test
     public void WHEN_intValueIs_28_THEN_convertFromIntToBinary_returnsBinaryRepresentationOfIntValue(){
         //Arrange
@@ -91,7 +249,7 @@ public class TP2PartieATest
         final char[] EXPECTED_BINARY_ARRAY = {'1','0','0','0','1'};
         assertArrayEquals(EXPECTED_BINARY_ARRAY, binaryArray);
     }    
-
+/*
     //convertCharToBinary
     @Test
     public void WHEN_charIs_a_THEN_convertCharToBinary_returnsBinaryRepresentationOfChar(){
@@ -231,5 +389,5 @@ public class TP2PartieATest
         //Assert
         final char EXPECTED_CHAR_VALUE = '+';
         assertEquals(EXPECTED_CHAR_VALUE, charValue);
-    }
+    }*/
 }
