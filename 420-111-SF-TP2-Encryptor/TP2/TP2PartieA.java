f02d8810cd8190a618ebb70e6f81f1fea37913ef
SylvieAllain <sylvie_allain93@hotmail.com>
2018-11-23 13:29:38 -0500

    projet algo

---
+++ 420-111-SF-TP2-Encryptor/TP2/TP2PartieA.java
@@ -0,0 +1,13 @@
+ 
+
+
+public class TP2PartieA
+{
+  static char transformASCII_IntToChar(int value){ 
+      return (char)value;
+  }
+    
+  static int transformASCII_CharToInt(char c){
+      return (int)c;
+  }   
+}
