81bd14c2bb40d16ee3bfa687474e67d53492c3c1
SylvieAllain <sylvie_allain93@hotmail.com>
2018-11-30 14:49:16 -0500

    Partie D

--- 420-111-SF-TP2-Encryptor/TP2/TP2PartieD.java
+++ 420-111-SF-TP2-Encryptor/TP2/TP2PartieD.java
@@ -1,6 +1,8 @@
 
 public class TP2PartieD
 {
+    static int NIBBLE = 4;
+    
     static String mainCipher(String msg, String key, boolean isEncrypting)
     {
         char[] msgCharArray = msg.toCharArray();
@@ -9,17 +11,154 @@ public class TP2PartieD
         char[] rep = null;
         if(isEncrypting)
         {
-            //char[] binaryCipher = TP2PartieC.encryptXOR(msgCharArray, keyCharArray);
-            //rep = convertBinaryArrayToHexArray(binaryCipher);
+            char[] binaryCipher = TP2PartieC.encryptXOR(msgCharArray, keyCharArray);
+            rep = convertBinaryArrayToHexArray(binaryCipher);
         }
         else
         {
-            //char[] binaryCipher = convertHexArrayToBinaryArray(msgCharArray);           
-            //rep = TP2PartieC.decryptXOR(binaryCipher, keyCharArray);
+            char[] binaryCipher = convertHexArrayToBinaryArray(msgCharArray);           
+            rep = TP2PartieC.decryptXOR(binaryCipher, keyCharArray);
         }
         String s = new String(rep);
 
         return s;
+    }
+    
+    static char switchBinaryToHex ( char [] binaryArray){
+        char hexChar = '0';
+        int binaryToInt = TP2PartieA.convertFromBinaryToInt(binaryArray); 
+        
+        switch (binaryToInt){
+            case 1:
+                hexChar = '1';
+                break;
+            case 2 :
+                hexChar = '2';
+                break;
+            case 3 : 
+                hexChar = '3';
+                break;
+            case 4 :
+                hexChar = '4';
+                break;
+            case 5 :
+                hexChar = '5';
+                break;
+            case 6 :
+                hexChar = '6';
+                break;
+            case 7 :
+                hexChar = '7';
+                break;
+            case 8 :
+                hexChar = '8';
+                break;
+            case 9 :
+                hexChar = '9';
+                break;
+            case 10 :
+                hexChar = 'A';
+                break;
+            case 11 :
+                hexChar = 'B';
+                break;
+            case 12 : 
+                hexChar = 'C';
+                break;
+            case 13 : 
+                hexChar = 'D';
+                break;
+            case 14 :
+                hexChar = 'E';
+                break;
+            case 15 : 
+                hexChar = 'F';
+                break;
+        }
+        
+        return hexChar;
+    }
+    
+    static char [] switchHexToBinaryArray ( char hexChar){
+        int intChar = 0;
+        
+        switch (hexChar){
+            case '1' :
+                intChar = 1;
+                break;
+            case '2' :
+                intChar = 2;
+                break;
+            case '3' :
+                intChar = 3;
+                break;
+            case '4' : 
+                intChar = 4;
+                break;
+            case '5' : 
+                intChar = 5;
+                break;
+            case '6' : 
+                intChar = 6;
+                break;
+            case '7' : 
+                intChar = 7;
+                break;
+            case '8' : 
+                intChar = 8;
+                break;
+            case '9' : 
+                intChar = 9;
+                break;
+            case 'A' : 
+                intChar = 10;
+                break;
+            case 'B' :
+                intChar = 11;
+                break;
+            case 'C' : 
+                intChar = 12;
+                break;
+            case 'D' : 
+                intChar = 13;
+                break;
+            case 'E' : 
+                intChar = 14;
+                break;
+            case 'F' : 
+                intChar = 15;
+                break;
+        }
+        char [] binaryArray = TP2PartieA.convertFromIntToBinary (intChar, NIBBLE);
+        
+        return binaryArray;
+    }
+    
+    static char [] convertBinaryArrayToHexArray (char [] binaryArray){
+        int sizeHexArray = binaryArray.length / NIBBLE;
+        char [] hexArray = new char [sizeHexArray];
+        char [] nibbleBinaryArray = new char [NIBBLE];
+        for (int i = 0; i < hexArray.length; i++){
+            for (int j = 0; j < NIBBLE;j++){
+                nibbleBinaryArray [j] = binaryArray [j + (i * NIBBLE)];
+            }
+            hexArray [i] = switchBinaryToHex (nibbleBinaryArray);
+        }
+        
+        return hexArray;
+    }
+    
+    static char [] convertHexArrayToBinaryArray (char [] hexArray){
+        int sizeBinaryArray = hexArray.length * NIBBLE;
+        char [] binaryArray = new char [sizeBinaryArray];
+        for (int i = 0; i < hexArray.length; i++){
+            char [] charBinaryArray = switchHexToBinaryArray (hexArray [i]);
+            for (int j = 0; j < NIBBLE; j++){
+                binaryArray [j + (i * NIBBLE)] = charBinaryArray [j]; 
+            }
+        }
+        
+        return binaryArray;
     }
 }
                
--- 420-111-SF-TP2-Encryptor/TP2/TP2PartieDTest.java
+++ 420-111-SF-TP2-Encryptor/TP2/TP2PartieDTest.java
@@ -6,5 +6,241 @@ import org.junit.Test;
 
 public class TP2PartieDTest
 {
+    //Tests de la fonction switchBinaryToHex;
+    @Test
+    public void WHEN_binaryArrayRepresentInt_0_THEN_switchBinaryToHex_returnsHexChar_0(){
+        //Arrange
+        char [] binaryArray = {'0','0','0','0'};
+        
+        //Act
+        char hexChar = TP2PartieD.switchBinaryToHex (binaryArray);
+        
+        //Assert
+        final char EXPECTED_HEX_CHAR = '0';
+        assertEquals (EXPECTED_HEX_CHAR, hexChar);
+    }
     
+    @Test
+    public void WHEN_binaryArrayRepresentInt_1_THEN_switchBinaryToHex_returnsHexChar_1(){
+        //Arrange
+        char [] binaryArray = {'0','0','0','1'};
+        
+        //Act
+        char hexChar = TP2PartieD.switchBinaryToHex (binaryArray);
+        
+        //Assert
+        final char EXPECTED_HEX_CHAR = '1';
+        assertEquals (EXPECTED_HEX_CHAR, hexChar);
+    }
+    
+    @Test
+    public void WHEN_binaryArrayRepresentInt_5_THEN_switchBinaryToHex_returnsHexChar_5(){
+        //Arrange
+        char [] binaryArray = {'0','1','0','1'};
+        
+        //Act
+        char hexChar = TP2PartieD.switchBinaryToHex (binaryArray);
+        
+        //Assert
+        final char EXPECTED_HEX_CHAR = '5';
+        assertEquals (EXPECTED_HEX_CHAR, hexChar);
+    }
+    
+     @Test
+    public void WHEN_binaryArrayRepresentInt_10_THEN_switchBinaryToHex_returnsHexChar_A(){
+        //Arrange
+        char [] binaryArray = {'1','0','1','0'};
+        
+        //Act
+        char hexChar = TP2PartieD.switchBinaryToHex (binaryArray);
+        
+        //Assert
+        final char EXPECTED_HEX_CHAR = 'A';
+        assertEquals (EXPECTED_HEX_CHAR, hexChar);
+    }
+    
+     @Test
+    public void WHEN_binaryArrayRepresentInt_15_THEN_switchBinaryToHex_returnsHexChar_F(){
+        //Arrange
+        char [] binaryArray = {'1','1','1','1'};
+        
+        //Act
+        char hexChar = TP2PartieD.switchBinaryToHex (binaryArray);
+        
+        //Assert
+        final char EXPECTED_HEX_CHAR = 'F';
+        assertEquals (EXPECTED_HEX_CHAR, hexChar);
+    }
+    
+    //Tests de la fonction convertBinaryArrayToHexArray 
+    @Test
+    public void WHEN_binaryArrayRepresentOneHexChar_THEN_convertBinaryArrayToHexArray (){
+        //Arrange
+        char [] binaryArray = {'0','0','0','0'};
+        
+        //Act
+        char [] hexArray = TP2PartieD.convertBinaryArrayToHexArray(binaryArray);
+        
+        //Assert
+        final char [] EXPECTED_HEX_ARRAY = {'0'};
+        assertArrayEquals (EXPECTED_HEX_ARRAY,hexArray);
+    }
+    
+    @Test
+    public void WHEN_binaryArrayRepresentTwoHexChar_THEN_convertBinaryArrayToHexArray (){
+        //Arrange
+        char [] binaryArray = {'0','0','0','0','1','1','1','1'};
+        
+        //Act
+        char [] hexArray = TP2PartieD.convertBinaryArrayToHexArray(binaryArray);
+        
+        //Assert
+        final char [] EXPECTED_HEX_ARRAY = {'0','F'};
+        assertArrayEquals (EXPECTED_HEX_ARRAY,hexArray);
+    }
+    
+    @Test
+    public void WHEN_binaryArrayRepresentManyHexChar_THEN_convertBinaryArrayToHexArray (){
+        //Arrange
+        char [] binaryArray = {'0','0','0','0','1','1','1','1','0','0','0','0','1','1','1','1'};
+        
+        //Act
+        char [] hexArray = TP2PartieD.convertBinaryArrayToHexArray(binaryArray);
+        
+        //Assert
+        final char [] EXPECTED_HEX_ARRAY = {'0','F','0','F'};
+        assertArrayEquals (EXPECTED_HEX_ARRAY,hexArray);
+    }
+    
+    @Test
+    public void WHEN_binaryArrayRepresentManyHexCharIncludingAn_A_THEN_convertBinaryArrayToHexArray (){
+        //Arrange
+        char [] binaryArray = {'0','0','0','0','1','1','1','1','1','0','1','0','0','0','0','0','1','1','1','1'};
+        
+        //Act
+        char [] hexArray = TP2PartieD.convertBinaryArrayToHexArray(binaryArray);
+        
+        //Assert
+        final char [] EXPECTED_HEX_ARRAY = {'0','F','A','0','F'};
+        assertArrayEquals (EXPECTED_HEX_ARRAY,hexArray);
+    }
+    
+    //Tests de la fonction switchHexToArray 
+    @Test
+    public void WHEN_binaryArrayRepresentInt_0_THEN_switchHexToBinaryArray_returnsHexChar_0(){
+        //Arrange
+        char hexChar = '0';
+        
+        //Act
+        char [] binaryArray = TP2PartieD.switchHexToBinaryArray (hexChar);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','0'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+    @Test
+    public void WHEN_hexCharRepresent_1_THEN_switchHexToBinaryArray_returnsBinaryArray(){
+        //Arrange
+        char hexChar = '1';
+        
+        //Act
+        char [] binaryArray = TP2PartieD.switchHexToBinaryArray (hexChar);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+    @Test
+    public void WHEN_hexCharRepresent_5_THEN_switchHexToBinaryArray_returnsBinaryArray(){
+        //Arrange
+        char hexChar = '5';
+        
+        //Act
+        char [] binaryArray = TP2PartieD.switchHexToBinaryArray (hexChar);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','1','0','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+     @Test
+    public void WHEN_hexCharRepresent_A_THEN_switchHexToBinaryArray_returnsBinaryArray(){
+        //Arrange
+        char hexChar = 'A';
+        
+        //Act
+        char [] binaryArray = TP2PartieD.switchHexToBinaryArray (hexChar);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'1','0','1','0'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+     @Test
+    public void WHEN_hexCharRepresent_F_THEN_switchHexToBinaryArray_returnsBinaryArray(){
+        //Arrange
+        char hexChar = 'F';
+        
+        //Act
+        char [] binaryArray = TP2PartieD.switchHexToBinaryArray (hexChar);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'1','1','1','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+    //Tests de la fonction convertHexArrayToBinaryArray
+    @Test
+    public void WHEN_hexArrayRepresentOneBinaryNibble_THEN_convertHexArrayToBinaryArray_ReturnBinaryArray (){
+        //Arrange
+        char [] hexArray = {'0'};
+        
+        //Act
+        char [] binaryArray = TP2PartieD.convertHexArrayToBinaryArray (hexArray);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','0'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+    @Test
+    public void WHEN_hexArrayRepresentTwoBinaryNibble_THEN_convertHexArrayToBinaryArray_ReturnBinaryArray (){
+        //Arrange
+        char [] hexArray = {'0','F'};
+        
+        //Act
+        char [] binaryArray = TP2PartieD.convertHexArrayToBinaryArray (hexArray);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','0','1','1','1','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+    @Test
+    public void WHEN_hexArrayRepresentManyBinaryNibble_THEN_convertHexArrayToBinaryArray_ReturnBinaryArray (){
+        //Arrange
+        char [] hexArray = {'0','F','0','F'};
+        
+        //Act
+        char [] binaryArray = TP2PartieD.convertHexArrayToBinaryArray (hexArray);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','0','1','1','1','1','0','0','0','0','1','1','1','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
+    
+     @Test
+    public void WHEN_hexArrayIncludingAn_A_RepresentManyBinaryNibble_THEN_convertHexArrayToBinaryArray_ReturnBinaryArray (){
+        //Arrange
+        char [] hexArray = {'0','F','A','0','F'};
+        
+        //Act
+        char [] binaryArray = TP2PartieD.convertHexArrayToBinaryArray (hexArray);
+        
+        //Assert
+        final char [] EXPECTED_BINARY_ARRAY = {'0','0','0','0','1','1','1','1','1','0','1','0','0','0','0','0','1','1','1','1'};
+        assertArrayEquals (EXPECTED_BINARY_ARRAY, binaryArray);
+    }
 }
